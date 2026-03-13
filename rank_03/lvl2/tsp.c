f7e6363291b8c8954202db6e8ac432150c9755a0
Daryna Shvydka <dashvydk@2-D-1.42heilbronn.de>
2026-03-13 14:06:54 +0100

    added tsp solution

--- rank_03/lvl2/tsp.c
+++ rank_03/lvl2/tsp.c
@@ -1,48 +1,51 @@
 #include <math.h>
 #include <stdio.h>
 
-float	x[11], y[11];
-int n, used[11];
-float	min_dist = 1e9;
+float x[15];
+float y[15];
+int n = 0;
+int visited[15];
+float min_dist = 1000000000.0;
 
-float	dist(int i, int j)
-{
-	return (sqrtf((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i]
-				- y[j])));
+float calc_dist(float x1, float y1, float x2, float y2) {
+	float x_diff = x1 - x2;
+	float y_diff = y1 - y2;
+	return (sqrtf((x_diff * x_diff) + (y_diff * y_diff)));
 }
 
-void	solve(int pos, int count, float cost)
-{
-	if (cost >= min_dist)
-		return ;
-	if (count == n)
-	{
-		cost += dist(pos, 0);
-		if (cost < min_dist)
-			min_dist = cost;
-		return ;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		if (!used[i])
-		{
-			used[i] = 1;
-			solve(i, count + 1, cost + dist(pos, i));
-			used[i] = 0;
+void solve(int current_city, int count, float current_dist) {
+	int i;
+
+	if(count == n) {
+		float drive_home = calc_dist(x[current_city], y[current_city], x[0], y[0]);
+		float total_trip = current_dist + drive_home;
+		if (total_trip < min_dist) {
+			min_dist = total_trip;
 		}
+		return;
+	}
+	
+	i = 0;
+	while(i < n) {
+		if(visited[i] == 0) {
+			visited[i] = 1; // Place (Lock)
+			solve(i, count + 1, current_dist + calc_dist(x[current_city], y[current_city], x[i], y[i])); // Recurse
+			visited[i] = 0; // Undo (Backtrack)
+		}
+		i++;
 	}
 }
 
-int	main(void)
-{
-	n = 0;
-	while (n < 11 && scanf("%f, %f", &x[n], &y[n]) == 2)
+int main (void) {
+	while (fscanf(stdin, "%f, %f", &x[n], &y[n]) == 2) 
 		n++;
+		
+	if (n == 0) // Edge case: if the file is empty!
+		return (0);
 
-	used[0] = 1;
-	solve(0, 1, 0);
-	if (n == 0)
-		min_dist = 0;
+	visited[0] = 1;
+	solve(0, 1, 0.0);
 	printf("%.2f\n", min_dist);
+	
 	return (0);
 }
